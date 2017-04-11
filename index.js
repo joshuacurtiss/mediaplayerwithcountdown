f6f753ccb5954ac446967ffd73734b3bb1b0f9d8
Josh Curtiss <git@curtiss.me>
2017-04-11 00:53:35 -0500

    Add support for images, not just videos.
    See #1. Images do not fade, though.

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ while ( typeof config=="undefined" && ++configTries<2 );
 function displaySettings() {
     $("#settingsForm")
         .find("#frmVideoDirectory").val(config.videoDirectory).end()
+        .find("#frmImageDuration").val(config.imageDuration).end()
         .find("#frmTime").val(config.time).end()
         .find("#frmPhase2threshold").val(config.phase2threshold).end()
         .find("#frmPhase1threshold").val(config.phase1threshold).end()
@@ -45,6 +46,7 @@ function saveSettings() {
     hideSettings();
     config.time=$("#frmTime").val();
     config.videoDirectory=$("#frmVideoDirectory").val();
+    config.imageDuration=$("#frmImageDuration").val();
     config.phase2threshold=$("#frmPhase2threshold").val();
     config.phase1threshold=$("#frmPhase1threshold").val();
     config.doneVideo=$("#frmDoneVideo").val();
@@ -62,6 +64,9 @@ var phase=9;
 var timer;
 var videos=[];
 var goal;
+var imageTimeout;
+const VIDEO_EXTS=["m4v","mov","mp4","webm"];
+const IMAGE_EXTS=["png","jpg","jpeg","gif","bmp"];
 
 // Functions
 function setPhase(ph) {
@@ -75,6 +80,14 @@ function setPhase(ph) {
 function getRandomInt(min, max) { // Returns a random number between min (inclusive) and max (exclusive)
     return Math.floor(Math.random() * (max - min)) + min;
 }
+function isImage(f) {
+    var ext=f.split(".").pop().toLowerCase();
+    return (IMAGE_EXTS.indexOf(ext)>=0);
+}
+function isVideo(f) {
+    var ext=f.split(".").pop().toLowerCase();
+    return (VIDEO_EXTS.indexOf(ext)>=0);
+}
 function updateTimer() {
     var now=new moment();
     var diff=moment.duration(goal.diff(now));
@@ -87,10 +100,25 @@ function updateTimer() {
     if( secs>0 ) timer=setTimeout(updateTimer,996);
 }
 function setVideo(f) {
+    if(imageTimeout) clearTimeout(imageTimeout);
+    // Calculate path for media (regardless of image/video)
     var path=`${config.videoDirectory}/${f}`;
+    if( isImage(f) ) path=`url("file://${path}")`;
+    // Get the previous path
     var $elem=$("#video");
-    var success=($elem.attr("src")!=path || videos.length==1);
-    if( success ) $elem.attr("src",path);
+    var prevPath=$elem.attr("src");
+    if( !prevPath ) prevPath=$elem.css("background-image");
+    // Determine if this was successful. If same as prev, that's not successful! Unless there's only one media item.
+    var success=(prevPath!=path || videos.length==1);
+    // Set the media!
+    if( success && isImage(f) ) {
+        $elem.css("background-image",path).attr("src","");
+        imageTimeout=setTimeout(loadRandomVideo,config.imageDuration*1000);
+    } else if( success && isVideo(f) ) {
+        $elem.attr("src",path).css("background-image","none");
+    } else {
+        success=false;
+    }
     return success;
 }
 function loadRandomVideo() {
